4a58826959c101801264ab5e6854980dd0806698
knacko <geekbike@hotmail.com>
2025-10-03 12:24:29 -0600

    feat: reports generate

--- file_compare_tool/functions.py
+++ file_compare_tool/functions.py
@@ -10,19 +10,21 @@ def compare(folder1, folder2, yaml):
     print ("blah")
 
 def compareFile(file1, file2, ids, columns):    
-    folder1 = st.importToDataFrame(file1)[ids + columns]
-    folder2 = st.importToDataFrame(file2)[ids + columns]
-    cmp = Comparison(folder1, folder2, join_columns=ids)
+    data1 = st.importToDataFrame(file1)[ids + columns]
+    data2 = st.importToDataFrame(file2)[ids + columns]
+    cmp = Comparison(data1, data2, join_columns=ids)
     return cmp
 
 def main(folder1, folder2, yaml_path):
 
     with open(yaml_path, 'r') as f:
-        data = yaml.load(f, Loader=yaml.SafeLoader).get("files")
+        load = yaml.load(f, Loader=yaml.SafeLoader)        
+        metadata = load.get("metadata")
+        files = load.get("files")
 
     results = []
 
-    for file in data:
+    for file in files:
         file1 = st.findFiles(file.get("path"), target_directory = folder1)
         if (len(file1) > 1): raise LookupError(f"Duplicate files found in folder: {file1}")
         
@@ -33,18 +35,26 @@ def main(folder1, folder2, yaml_path):
         cols = [item.strip() for item in file.get("columns").split(',')]
 
         cmp = compareFile(file1[0], file2[0], ids, cols).diverging_subset()
-        if not cmp.empty: results.append(cmp)
 
-        print("test")
+        if not cmp.empty: results.append((file.get("path"),cmp))
 
-    print("blah")
+    generateReport(metadata, results)
 
+def generateReport(metadata, results):
 
-def file_choices(choices,fname):
-    ext = os.path.splitext(fname)[1][1:]
-    if ext not in choices:
-       parser.error("file doesn't end with one of {}".format(choices))
-    return fname
+    report = (
+        f"{metadata.get('title')}\n"
+        f"{metadata.get('folder1_name')} ==> {metadata.get('folder2_name')}"
+    )
+
+    if results:
+        for result in results:
+            report = f"{report}\n\n{result[0]}\n{result[1].to_string(index=False)}"
+    else :
+        report = f"{report}\nNo errors detected"
+
+    print (report)
+    print("bleh")
 
 def yaml_type(p):
     ext = Path(p).suffix
